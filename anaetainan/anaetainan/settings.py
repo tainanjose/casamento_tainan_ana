87a2a491af3f9652466a6c010765bf60a2d9fcd2
Roger Camargo <huogerac@gmail.com>
2024-03-27 20:52:38 -0300

    Fix model de fotos e dependencia do floppyforms

--- anaetainan/anaetainan/settings.py
+++ anaetainan/anaetainan/settings.py
@@ -64,6 +64,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     "django_extensions",
     "whitenoise.runserver_nostatic",
+    "floppyforms",
 ]
 
 # CORS
--- anaetainan/fotos/models.py
+++ anaetainan/fotos/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext as _
 
 class Fotos(models.Model):
     titulo = models.CharField(_("Titulo"), max_length=128, blank=True, default="")
-    imagem = models.ImageField(_("imagem"), upload_to="fotos/", blank=True, default="")
+    imagem = models.URLField(_("imagem"), blank=True, default="", max_length=1024)
     ordem = models.IntegerField(blank=True, default=1, null=True)
 
     class Meta:
