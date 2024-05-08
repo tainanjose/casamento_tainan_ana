af7c3d5cba7d2840e4f04bd5960101dfa8c27875
Roger Camargo <huogerac@gmail.com>
2024-05-08 15:53:22 -0300

    Permite adicionar imagem como string Issue #10

--- anaetainan/fotos/models.py
+++ anaetainan/fotos/models.py
@@ -6,6 +6,7 @@ from django.utils.translation import gettext as _
 class Fotos(models.Model):
     titulo = models.CharField(_("Titulo"), max_length=128, blank=True, default="")
     imagem = models.URLField(_("imagem"), blank=True, default="", max_length=1024)
+    imagem_data = models.TextField(_("imagem data"), blank=True, null=True)
     ordem = models.IntegerField(blank=True, default=1, null=True)
 
     class Meta:
@@ -14,3 +15,7 @@ class Fotos(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    @property
+    def imagem_src(self):
+        return self.imagem_data if self.imagem_data else self.imagem
